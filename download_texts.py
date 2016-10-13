a41a82dcd3e26843900c8da855f43f20ebced823
benstox <benstox@gmail.com>
2016-10-13 17:13:07 +0100

    Better download_texts.py so that data can be imported into the javascript.

--- download_texts.py
+++ download_texts.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import re
 import requests
 
 from bs4 import BeautifulSoup
@@ -16,8 +17,12 @@ pages = {
          "css": "body > p:nth-of-type(3)"},
     "augustine_conf_i":
         {"url": "http://www.thelatinlibrary.com/augustine/conf1.shtml",
-         "css": "body"}}
+         "css": "body"},
+    "voragine_leg_aur_georgio":
+         {"url": "http://www.thelatinlibrary.com/voragine/georgio.shtml",
+          "css": "body"}}
 
+script_tags = []
 for to_do, page in pages.items():
     r = requests.get(page["url"])
     s = BeautifulSoup(r.content, "lxml")
@@ -26,5 +31,15 @@ for to_do, page in pages.items():
     tokenizer = RegexpTokenizer(r'[a-zA-Z]+')  # [\w]+ leaves numbers in
     tokens = tokenizer.tokenize(text)
 
-    with open(os.path.join(directory, to_do + ".txt"), "w") as textfile:
-        textfile.write(" ".join(tokens))
+    file = os.path.join(directory, to_do + ".js")
+    with open(file, "w") as textfile:
+        textfile.write('var {} = "'.format(to_do) + " ".join(tokens) + '";')
+
+    script_tags.append('<script class="training-text" src="{}"></script>'.format(file))
+
+with open("page_template.html", "r") as template_file:
+    template = template_file.read()
+
+template = re.sub("\{\{ text_files \}\}", '\n    '.join(script_tags), template)
+with open("page.html", "w") as output:
+    output.write(template)
